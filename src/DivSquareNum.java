7be4a5d7f45d89392f5a9ff683a2d5c6b137d64a
지민 백 <s2104@e-mirim.hs.kr>
2023-01-11 16:44:31 +0900

    Math.pow() 대신 더 간단한 방법인 Math.sqrt()메서드로 문제 풀이

--- src/DivSquareNum.java
+++ src/DivSquareNum.java
@@ -2,9 +2,9 @@
 public class DivSquareNum {
     public static void main(String[] args) {
         Solution2 s = new Solution2();
-//        System.out.println(s.solution(144));
-//        System.out.println(s.solution(976));
-//        System.out.println(s.solution(1));
+        System.out.println(s.solution(144));
+        System.out.println(s.solution(976));
+        System.out.println(s.solution(1));
         System.out.println(s.solution(1000));
     }
 }
@@ -18,6 +18,9 @@ class Solution2{
 //        return answer;
 
         //2> Math.pow()메서드 이용해서 풀기
-        return (int)Math.pow( (int)Math.pow(n, 0.5) ,2)==n ? 1 : 2;
+//        return (int)Math.pow( (int)Math.pow(n, 0.5) ,2)==n ? 1 : 2;
+
+        //3> Math.sqrt()메서드 이용해서 풀기
+        return n%Math.sqrt(n)==0 ? 1 : 2;
     }
 }
