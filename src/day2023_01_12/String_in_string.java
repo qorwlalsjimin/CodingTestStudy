217177b822edde2c2fc7719e6f72194fcf5dbfeb
지민 백 <s2104@e-mirim.hs.kr>
2023-01-12 15:45:20 +0900

    contains 메서드로 간단하게 문자열 찾아서 풀기

---
+++ src/day2023_01_12/String_in_string.java
@@ -0,0 +1,18 @@
+package day2023_01_12;
+
+//문자열안에 문자열
+//문제 https://school.programmers.co.kr/learn/courses/30/lessons/120908
+//블로그
+public class String_in_string {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("ab6CDE443fgh22iJKlmn1o", "6CD"));
+    }
+}
+
+class Solution {
+    public int solution(String str1, String str2) {
+        //1> contains 메서드로 간단하게 풀기
+        return str1.contains(str2) ? 1 : 2;
+    }
+}
