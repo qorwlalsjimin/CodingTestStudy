1e16937e49fb34b0323330ca5710ff1a63b7b612
지민 백 <s2104@e-mirim.hs.kr>
2023-01-17 23:45:13 +0900

    substring 메서드로 문제 풀기

---
+++ src/day2023_01_17/ChangeIndex.java
@@ -0,0 +1,16 @@
+package day2023_01_17;
+
+public class ChangeIndex {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("hello", 1, 2)); // h e l l o
+        System.out.println(s.solution("I love you", 3, 6)); // I love you => I l veoyou
+
+    }
+}
+
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        return my_string.substring(0, num1)+my_string.charAt(num2)+my_string.substring(num1+1, num2)+my_string.charAt(num1)+my_string.substring(num2, my_string.length());
+    }
+}
