9e347109156451b7136d0cf210bd1368de6a7ea2
지민 백 <s2104@e-mirim.hs.kr>
2023-01-19 17:04:13 +0900

    주석 추가

--- src/day2023_01_17/ChangeIndex.java
+++ src/day2023_01_17/ChangeIndex.java
@@ -1,10 +1,14 @@
 package day2023_01_17;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+//인덱스 바꾸기
+// 문제 https://school.programmers.co.kr/learn/courses/30/lessons/120895
+// 블로그 https://qiwisil.tistory.com/43
 public class ChangeIndex {
     public static void main(String[] args) {
         Solution s = new Solution();
@@ -17,15 +21,30 @@ public class ChangeIndex {
 class Solution {
     public String solution(String my_string, int num1, int num2) {
 //        //1> char 배열 이용
-//        char[] tempArr = my_string.toCharArray();
+//        char[] tempArr = my_string.toCharArray(); //문자열 to char[]
+//
+//        //swap
 //        char temp = tempArr[num1];
 //        tempArr[num1] = tempArr[num2];
 //        tempArr[num2] = temp;
-//        return String.valueOf(tempArr);
+//
+//        return String.valueOf(tempArr); //char[] to 문자열
+
+
 
 //        //2> substring() 메서드 이용
-//        return my_string.substring(0, num1)+my_string.charAt(num2)+my_string.substring(num1+1, num2)+my_string.charAt(num1)+my_string.substring(num2, my_string.length());
+//        return my_string.substring(0, num1)                     //num1 전까지
+//                +my_string.charAt(num2)                         //num2에 해당하는 문자
+//                +my_string.substring(num1+1, num2)              //num1 다음부터 ~ num2전까지
+//                +my_string.charAt(num1)                         //num1에 해당하는 문자
+//                +my_string.substring(num2, my_string.length()); //num2 다음부터 ~ 끝까지
 
+        List<String> list1 = new ArrayList<>();
+        list1.add("a");
+        list1.add("b");
+        list1.add("c");
+        Collections.swap(list1, 0, 2);
+        System.out.println(String.join("*", list1));
         //3> Collections.swap 메서드 이용
         List<String> list = Arrays.stream(my_string.split("")).collect(Collectors.toList()); //문자열 to List
         Collections.swap(list, num1, num2); //swap
