ef2ddeb2ec2401f7d95fd0513e0f98d598671828
지민 백 <s2104@e-mirim.hs.kr>
2023-01-19 16:43:28 +0900

    List, Stream, Collections.swap()으로 문제 풀이

--- src/day2023_01_17/ChangeIndex.java
+++ src/day2023_01_17/ChangeIndex.java
@@ -1,5 +1,10 @@
 package day2023_01_17;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ChangeIndex {
     public static void main(String[] args) {
         Solution s = new Solution();
@@ -11,14 +16,19 @@ public class ChangeIndex {
 
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        //1> char 배열 이용
-        char[] tempArr = my_string.toCharArray();
-        char temp = tempArr[num1];
-        tempArr[num1] = tempArr[num2];
-        tempArr[num2] = temp;
-        return String.valueOf(tempArr);
+//        //1> char 배열 이용
+//        char[] tempArr = my_string.toCharArray();
+//        char temp = tempArr[num1];
+//        tempArr[num1] = tempArr[num2];
+//        tempArr[num2] = temp;
+//        return String.valueOf(tempArr);
 
-        //2> substring() 메서드 이용
+//        //2> substring() 메서드 이용
 //        return my_string.substring(0, num1)+my_string.charAt(num2)+my_string.substring(num1+1, num2)+my_string.charAt(num1)+my_string.substring(num2, my_string.length());
+
+        //3> Collections.swap 메서드 이용
+        List<String> list = Arrays.stream(my_string.split("")).collect(Collectors.toList()); //문자열 to List
+        Collections.swap(list, num1, num2); //swap
+        return String.join("", list); //List to 문자열
     }
 }
