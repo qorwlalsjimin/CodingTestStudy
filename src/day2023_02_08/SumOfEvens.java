d9f3dcdfa7e0caa590f03dd6f439926796cc34a2
지민 백 <s2104@e-mirim.hs.kr>
2023-01-30 15:54:38 +0900

    IntStream으로 문제 풀이

---
+++ src/day2023_02_08/SumOfEvens.java
@@ -0,0 +1,21 @@
+package day2023_02_08;
+
+import java.util.stream.IntStream;
+
+//짝수의 합
+// 문제 https://school.programmers.co.kr/learn/courses/30/lessons/120831
+// 블로그
+public class SumOfEvens {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(10));
+    }
+}
+
+class Solution {
+    public int solution(int n) {
+        return IntStream.range(2, n+1) //짝수의 시작인 2부터 n까지 스트림 생성
+                .filter(i -> i%2==0) //짝수만 남도록 필터링
+                .sum(); //남은 데이터들의 총합
+    }
+}
