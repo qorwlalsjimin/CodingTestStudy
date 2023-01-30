a6348f408297c2f626e5e0f1636d7f6f53e34471
지민 백 <s2104@e-mirim.hs.kr>
2023-01-30 15:30:54 +0900

    주석 처리

--- src/day2023_01_26/Factorial.java
+++ src/day2023_01_26/Factorial.java
@@ -1,5 +1,8 @@
 package day2023_01_26;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 //팩토리얼
 // 문제 https://school.programmers.co.kr/learn/courses/30/lessons/120848
 // 블로그
@@ -16,17 +19,17 @@ class Solution {
         int answer = 0;
 
         //이중 반복문
-        int temp = 1; //팩토리얼 계산값
-        for(int i = 10; i>0; i--){
-            temp = 1; //반복 돌때마다 1로 되돌아감
-            for(int j = 1; j<=i; j++)
-                temp *= j; //팩토리얼 계산
-            if(temp <= n) { //계산된 값이 n보다 작거나 같으면
-                // cf) temp는 반복할수록 값이 작아진다. => temp가 n보다 작아졌을때가 포인트
-                answer = i; //answer에 i 넣고
-                break; //멈춘다
-            }
-        }
+//        int temp = 1; //팩토리얼 계산값
+//        for(int i = 10; i>0; i--){
+//            temp = 1; //반복 돌때마다 1로 되돌아감
+//            for(int j = 1; j<=i; j++)
+//                temp *= j; //팩토리얼 계산
+//            if(temp <= n) { //계산된 값이 n보다 작거나 같으면
+//                // cf) temp는 반복할수록 값이 작아진다. => temp가 n보다 작아졌을때가 포인트
+//                answer = i; //answer에 i 넣고
+//                break; //멈춘다
+//            }
+//        }
 
         return answer;
     }
