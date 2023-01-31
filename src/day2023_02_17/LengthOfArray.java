c7b328c900e3763de58d93d55ac1f743b4c6601d
지민 백 <s2104@e-mirim.hs.kr>
2023-01-31 20:54:59 +0900

    Stream으로 반복문 없이 문제 풀기

--- src/day2023_02_17/LengthOfArray.java
+++ src/day2023_02_17/LengthOfArray.java
@@ -1,7 +1,9 @@
 package day2023_02_17;
 
+import java.util.Arrays;
+
 //배열 원소의 길이
-// 문제
+// 문제 https://school.programmers.co.kr/learn/courses/30/lessons/120854
 // 블로그
 public class LengthOfArray {
     public static void main(String[] args) {
@@ -15,11 +17,15 @@ public class LengthOfArray {
 
 class Solution {
     public int[] solution(String[] strlist) {
-        int[] answer = new int[strlist.length];
+//        int[] answer = new int[strlist.length];
+//
+//        for(int i = 0; i<strlist.length; i++)
+//            answer[i] = strlist[i].length();
+//
+//        return answer;
 
-        for(int i = 0; i<strlist.length; i++)
-            answer[i] = strlist[i].length();
-
-        return answer;
+        return Arrays.stream(strlist) //문자열 배열로 Stream 생성
+                .mapToInt(String::length) //int형으로 형변환, String 클래스의 length() 메서드
+                .toArray(); //배열로 바꾸기
     }
 }
