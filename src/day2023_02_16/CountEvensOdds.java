bedc9fb294ab56319f73a32f74f567087ef8818a
지민 백 <s2104@e-mirim.hs.kr>
2023-01-31 20:07:22 +0900

    IntStream으로 홀수 짝수 구분해보기

---
+++ src/day2023_02_16/CountEvensOdds.java
@@ -0,0 +1,31 @@
+package day2023_02_16;
+
+import java.util.stream.IntStream;
+
+//짝수 홀수 개수
+// 문제
+// 블로그
+public class CountEvensOdds {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = new int[]{1,2,3,4,5};
+        int[] answer;
+        answer = s.solution(arr);
+
+        for(int a : answer)
+            System.out.print(a+" ");
+    }
+}
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+
+        //1> IntStream을 이용한 풀이
+        answer[0] = (int)IntStream.of(num_list).filter(i -> i%2==0).count();
+        answer[1] = (int)IntStream.of(num_list).filter(i -> i%2==1).count();
+
+
+        return answer;
+    }
+}
