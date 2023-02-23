faee6c7ff3dd76c1b1797bde766c9b6c5a543db1
지민 백 <s2104@e-mirim.hs.kr>
2023-02-23 14:05:53 +0900

    repeat()함수로 간단하게 별 찍기

---
+++ src/day2023_02_23/RightTriangle.java
@@ -0,0 +1,16 @@
+package day2023_02_23;
+
+import java.util.Scanner;
+
+//직각삼각형 출력하기
+// 문제 https://school.programmers.co.kr/learn/courses/30/lessons/120823
+public class RightTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i<=n; i++)
+            System.out.println("*".repeat(i));
+
+    }
+}
