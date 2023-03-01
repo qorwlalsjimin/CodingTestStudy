5083425596beb205b2bc2ad356c451af52258f3c
지민 백 <s2104@e-mirim.hs.kr>
2023-03-02 00:10:32 +0900

    Stream으로 더 간편하게 풀기

--- src/day2023_03_02/RockScissorsPaper.java
+++ src/day2023_03_02/RockScissorsPaper.java
@@ -1,5 +1,9 @@
 package day2023_03_02;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 //가위 바위 보
 // 문제 https://school.programmers.co.kr/learn/courses/30/lessons/120839
 // 블로그
@@ -10,27 +14,34 @@ public class RockScissorsPaper {
     }
 }
 
+//가위 2
+//바위 0
+//보 5
 class Solution {
     public String solution(String rsp) {
-        String answer = "";
-        //가위 2
-        //바위 0
-        //보 5
 
-        for(char r : rsp.toCharArray()){
-            switch(r){
-                case '2':
-                    answer += "0";
-                    break;
-                case '0':
-                    answer += "5";
-                    break;
-                case '5':
-                    answer += "2";
-                    break;
-            }
-        }
+        /*Stream*/
+        List list = Arrays.stream(rsp.split("")) //문자열로 Stream 생성
+                .map(m -> m.equals("2") ? "0" : m.equals("0") ? "5" : "2") //2이면 0, 0이면 5고 아니면 2
+                .collect(Collectors.toList()); //Stream to List
+        return String.join("", list); //List to String
 
-        return answer;
+        /*switch문*/
+//        String answer = "";
+//        for(char r : rsp.toCharArray()){
+//            switch(r){
+//                case '2':
+//                    answer += "0";
+//                    break;
+//                case '0':
+//                    answer += "5";
+//                    break;
+//                case '5':
+//                    answer += "2";
+//                    break;
+//            }
+//        }
+//
+//        return answer;
     }
 }
