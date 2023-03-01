a77e4a98d267d111924c995534493d2f80ad2104
지민 백 <s2104@e-mirim.hs.kr>
2023-03-01 23:47:23 +0900

    초기화로 코드 줄이기, 다른 분 풀이 추가

--- src/day2023_01_27/Ant.java
+++ src/day2023_01_27/Ant.java
@@ -2,7 +2,7 @@ package day2023_01_27;
 
 //개미군단
 // 문제 https://school.programmers.co.kr/learn/courses/30/lessons/120837
-// 블로그
+// 블로그 https://qiwisil.tistory.com/61
 public class Ant {
     public static void main(String[] args) {
         Solution s = new Solution();
@@ -14,9 +14,8 @@ public class Ant {
 
 class Solution {
     public int solution(int hp) {
-        int answer = 0;
+        int answer = hp/5;
 
-        answer += hp/5;
         hp %= 5;
 
         answer += hp/3;
@@ -25,5 +24,10 @@ class Solution {
         answer += hp;
 
         return answer;
+
+        /*다른 분의 풀이*/
+        //return (hp/5) + ((hp%5) / 3) + ((hp%5) % 3);
+        //return (장군개미) + (병정개미) + (일개미)
+        //return (5로 나눈 몫) + ((장군개미 빼고 남은 개미수)/3) + (5로 나눈 나머지로 3을 나눠서 남은 나머지, /1해도 안 해도 같은 결과)
     }
 }
