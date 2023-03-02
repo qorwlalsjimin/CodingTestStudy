34e7ef1a06dfd34536035f15a06ac355465e79e6
지민 백 <s2104@e-mirim.hs.kr>
2023-03-02 17:25:05 +0900

    CharBuffer.wrap(), Character 클래스의 isDigit()과 getNumericValue()

---
+++ src/day2023_03_02/SortStr.java
@@ -0,0 +1,34 @@
+package day2023_03_02;
+
+import java.nio.CharBuffer;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+//문자열 정렬하기(1)
+// 문제 https://school.programmers.co.kr/learn/courses/30/lessons/120850
+// 블로그 https://qiwisil.tistory.com/65
+public class SortStr {
+    public static void main(String[] args) {
+        Solution3 s = new Solution3();
+        int[] answer = s.solution("p2o4i8gj2"); //2 4 8 2 // 2 2 4 8
+
+        for(int a : answer)
+            System.out.print(a+" ");
+
+    }
+}
+
+class Solution3 {
+    public int[] solution(String my_string) {
+        char[] chars = my_string.toCharArray(); //String to char[]
+
+        int[] answer = CharBuffer.wrap(chars).chars() //char[] to Stream
+                .filter(Character::isDigit) //숫자만 남기기
+                .map(Character::getNumericValue) //숫자의 아스키코드인 값('50')들을 실제 값(2)으로 바꾸어주기
+                .toArray(); //Stream to Array
+
+        Arrays.sort(answer); //정렬
+
+        return answer;
+    }
+}
