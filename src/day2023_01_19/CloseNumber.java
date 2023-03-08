9914001e454c648f2abd3cee9bcb548c8afdf4df
지민 백 <s2104@e-mirim.hs.kr>
2023-03-08 23:42:03 +0900

    Arrays.sort() 추가로 문제 풀어내기

--- src/day2023_01_19/CloseNumber.java
+++ src/day2023_01_19/CloseNumber.java
@@ -20,6 +20,7 @@ class Solution {
     public int solution(int[] array, int n) {
         int answer = 0;
         int min = 100; //원소의 최솟값: 1, n의 최댓값: 100 => 나올 수 있는 차의 최댓값: 99
+        Arrays.sort(array); //정렬 안 되어있는 array도 있음
         for(int a : array){
             int abs = Math.abs(n-a); //n과의 차이
             if(min > abs) { //answer보다 작으면
@@ -32,13 +33,3 @@ class Solution {
         return answer;
     }
 }
-
-//        원소 하나씩 다 돌면서
-//        n-원소의 절댓값을 만드시오! //abs
-//        그리고 절댓값의 최소값을 찾아...
-//
-//        여기서 주의할 점은!
-//        만약에 answer에 들어있는 값이랑 원소랑 절댓값이 똑같다면
-//        answer이랑 원소랑 크기 비교를 해서 원소가 더 크면 넣으면 안 되유
-//
-//        최소값을 만드는 원소를 answer에 넣어주세용
